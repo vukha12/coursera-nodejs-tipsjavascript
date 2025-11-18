d2a6f847840a1245d5518d4c268839acf56cd373
vukha12 <kha15964@gmail.com>
2025-11-18 15:21:00 +0700

    done section 8 (xủ lý response success)

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,10 +1,15 @@
 "use strict";
 
+import { CREATED } from "../core/success.response.js";
 import AccessService from "../services/access.service.js";
 
 class AccessController {
   signUp = async (req, res, next) => {
-    return res.status(201).json(await AccessService.signUp(req.body));
+    new CREATED({
+      message: "Regiserted OK!",
+      metadata: await AccessService.signUp(req.body),
+      options: { limit: 10 },
+    }).send(res);
   };
 }
 
