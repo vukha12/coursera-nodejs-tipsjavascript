433efce800718a7f243fc5b72ebbce576d7d192c
vukha12 <kha15964@gmail.com>
2026-01-13 20:34:53 +0700

    done section 48 (dead letter exchange) TTL - Time To Live

--- sys_message_queue_shopDEV/server.js
+++ sys_message_queue_shopDEV/server.js
@@ -1,9 +1,23 @@
 'use strict';
 
-const { consumerQueue } = require('./src/services/consumerQueue.service.js')
+const { consumerQueue,
+    consumerToQueueFailed,
+    consumerToQueueNormal } = require('./src/services/consumerQueue.service.js')
 const queueName = 'test-topic';
-consumerQueue(queueName).then(() => {
-    console.log(`Message consumer started ${queueName}`)
+// consumerQueue(queueName).then(() => {
+//     console.log(`Message consumer started ${queueName}`)
+// }).catch(err => {
+//     console.err(`Message Error:`, err.message)
+// })
+
+consumerToQueueNormal().then(() => {
+    console.log(`Message consumerToQueueNormal started `)
 }).catch(err => {
-    console.err(`Message Error:`, err.message)
+    console.error(`Message Error:`, err.message)
+})
+
+consumerToQueueFailed().then(() => {
+    console.log(`Message consumerToQueueFailed started `)
+}).catch(err => {
+    console.error(`Message Error:`, err.message)
 })
--- sys_message_queue_shopDEV/src/services/consumerQueue.service.js
+++ sys_message_queue_shopDEV/src/services/consumerQueue.service.js
@@ -5,6 +5,12 @@ const {
     connectToRabbitMQ
 } = require("../dbs/init.rabbit")
 
+const log = console.log
+
+console.log = function () {
+    log.apply(console, [new Date()].concat(arguments))
+}
+
 const messageService = {
     consumerQueue: async (queueName) => {
         try {
@@ -12,6 +18,52 @@ const messageService = {
             await consumerQueue(channel, queueName);
         } catch (error) {
             console.error(`Error consumerQueue:::`, error);
+        }
+    },
+
+    // case processing
+    consumerToQueueNormal: async () => {
+        try {
+            const { channel } = await connectToRabbitMQ();
+
+            const notiQueue = 'noti.queue';
+
+            const timeExpried = 15000
+            setTimeout(() => {
+                channel.consume(notiQueue, msg => {
+                    console.log(`Send notificationQueue successfully processed::`, msg.content.toString())
+                    channel.ack(msg);
+                })
+            }, timeExpried)
+
+        } catch (error) {
+            console.error(error)
+        }
+    },
+
+    // case failed processing
+    consumerToQueueFailed: async () => {
+        try {
+            const { channel } = await connectToRabbitMQ();
+
+            const notiExchangeDLX = 'noti.dlx';
+            const notiRoutingKeyDLX = 'noti.key.dlx';
+            const notiQueueHandler = 'noti.queue.hotfix';
+
+            await channel.assertExchange(notiExchangeDLX, 'direct', { durable: true })
+
+            const queueResult = await channel.assertQueue(notiQueueHandler,
+                {
+                    exclusive: false
+                })
+
+            await channel.bindQueue(queueResult.queue, notiExchangeDLX, notiRoutingKeyDLX);
+            await channel.consume(queueResult.queue, msgFailed => {
+                console.log(`this notification error, pls hot fix:::`, msgFailed.content.toString());
+            }, { noAck: true })
+        } catch (error) {
+            console.error(error);
+            throw error;
         }
     }
 }
