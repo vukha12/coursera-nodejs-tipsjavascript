5bdee5cd785cef3a33ee4bb8c30048492d72d083
vukha12 <kha15964@gmail.com>
2026-01-15 18:09:27 +0700

    done section 50 (DLE)

--- sys_message_queue_shopDEV/src/services/consumerQueue.service.js
+++ sys_message_queue_shopDEV/src/services/consumerQueue.service.js
@@ -28,14 +28,37 @@ const messageService = {
 
             const notiQueue = 'noti.queue';
 
-            const timeExpried = 15000
-            setTimeout(() => {
-                channel.consume(notiQueue, msg => {
-                    console.log(`Send notificationQueue successfully processed::`, msg.content.toString())
-                    channel.ack(msg);
-                })
-            }, timeExpried)
+            // 1. TTL
+            // const timeExpried = 15000
+            // setTimeout(() => {
+            //     channel.consume(notiQueue, msg => {
+            //         console.log(`Send notificationQueue successfully processed::`, msg.content.toString())
+            //         channel.ack(msg);
+            //     })
+            // }, timeExpried)
 
+
+            // 2. LOGIC
+            channel.consume(notiQueue, msg => {
+                try {
+                    const numberTest = Math.random();
+                    console.log(numberTest);
+                    if (numberTest < 0.8) {
+                        throw new Error('Send notification failed:: HOT FIX')
+                    }
+
+                    console.log(`SEND notificationQueue successfully processed: `, msg.content.toString())
+                    channel.ack(msg);
+                } catch (error) {
+                    console.error(`SEND notification error`, error);
+                    channel.nack(msg, false, false);
+                    /*
+                        nack: negative acknowledgement
+
+                    */
+                }
+
+            })
         } catch (error) {
             console.error(error)
         }
